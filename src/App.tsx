9efaa8d7e59e482402d7d1c0c8499014b5153ab2
ysuzuki19 <ysuzuki.cpp@gmail.com>
2021-09-14 00:15:24 +0900

    install muiv5 and set themeprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 const App: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={theme}></ThemeProvider>
+    </>
+  );
 };
 
 export default App;
